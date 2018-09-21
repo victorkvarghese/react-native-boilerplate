ed12b30ac4ba0fe5d85c6a9460f0ebd13f006426
Victor Varghese <victorvarghese148@gmail.com>
2018-09-21 07:09:40 +0530

    Added initialRouteName

--- app/navigation/NavigationStack.js
+++ app/navigation/NavigationStack.js
@@ -3,15 +3,20 @@ import { createStackNavigator } from 'react-navigation';
 import Login from 'app/screens/Login';
 import Home from 'app/screens/Home';
 
-const RNApp = createStackNavigator({
-    Login: {
-        screen: Login,
-        navigationOptions: { header: null, gesturesEnabled: false }
+const RNApp = createStackNavigator(
+    {
+        Login: {
+            screen: Login,
+            navigationOptions: { header: null, gesturesEnabled: false }
+        },
+        Home: {
+            screen: Home,
+            navigationOptions: { header: null, gesturesEnabled: false }
+        }
     },
-    Home: {
-        screen: Home,
-        navigationOptions: { header: null, gesturesEnabled: false }
+    {
+        initialRouteName: 'Login'
     }
-});
+);
 
 export default RNApp;
