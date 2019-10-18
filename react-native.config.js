396bf4849de34d3860e9538cf255767e92fb00eb
Kusal <shrstha.kusal@gmail.com>
2019-10-18 16:32:23 +0545

    RN > 0.60.0

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+// react-native.config.js
+module.exports = {
+  dependencies: {
+    // 'library name': { eg: react-native-config
+    //   platforms: {
+    //     set platform to null of manually setting up into your project.
+    //     ios: null, 
+    //     android: null
+    //   }
+    // },
+  }
+}
