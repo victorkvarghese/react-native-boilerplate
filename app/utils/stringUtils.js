4451b2a12e956e3a67f44b7c61b036022c44f9ac
Angelk90 <angelo.k90@hotmail.it>
2020-06-16 22:05:25 +0000

    Update stringUtils.js

--- app/utils/stringUtils.js
+++ app/utils/stringUtils.js
@@ -1,8 +1,6 @@
-export const getInitials = name => {
-  let initials = Array.prototype.map
-    .call(name.split(' '), function(x) {
-      return x.substring(0, 1).toUpperCase();
-    })
-    .join('');
-  return initials.substring(0, 2);
+export const getInitials = (name) => {
+    return Array.prototype.map
+      .call(name.split(' '), (x) => x.substring(0, 1).toUpperCase())
+      .join('')
+      .substring(0, 2);
 };
