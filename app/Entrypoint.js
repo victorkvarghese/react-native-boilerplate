39e324ec3447c283959526620ba87a35558c52f9
Angelk90 <angelo.k90@hotmail.it>
2020-06-16 21:57:23 +0000

    Update Entrypoint.js

--- app/Entrypoint.js
+++ app/Entrypoint.js
@@ -9,11 +9,11 @@ import { PersistGate } from 'redux-persist/es/integration/react';
 import {
   DarkTheme as PaperDarkTheme,
   DefaultTheme as PaperDefaultTheme,
-  Provider as PaperProvider,
+  Provider as PaperProvider
 } from 'react-native-paper';
 import {
   DefaultTheme as NavigationDefaultTheme,
-  DarkTheme as NavigationDarkTheme,
+  DarkTheme as NavigationDarkTheme
 } from '@react-navigation/native';
 
 import Navigator from 'app/navigation';
@@ -21,11 +21,16 @@ import configureStore from 'app/store/configureStore';
 
 const CombinedDefaultTheme = {
   ...PaperDefaultTheme,
-  ...NavigationDefaultTheme,
+  ...NavigationDefaultTheme
 };
+
 const CombinedDarkTheme = {
   ...PaperDarkTheme,
   ...NavigationDarkTheme,
+  colors: {
+    ...PaperDarkTheme.colors,
+    background: '#002b36'
+  }
 };
 
 const { persistor, store } = configureStore();
