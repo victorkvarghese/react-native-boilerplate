16e4e9187af9072531fc620daf1e8bfff6e81b37
Angelk90 <angelo.k90@hotmail.it>
2020-06-16 22:15:39 +0000

    Update createReducer.js

--- app/lib/createReducer.js
+++ app/lib/createReducer.js
@@ -4,10 +4,6 @@
  */
 export default function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action);
-    } else {
-      return state;
-    }
+    return handlers.hasOwnProperty(action.type) ? handlers[action.type](state, action) : state;
   };
 }
